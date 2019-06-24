7fe08f18914ff7939e5ae1e07c62ea73e8778732
Dave <Gillespie>
2019-06-23 23:00:59 -0400

    first commit, keypress works in console, not on browser yet

--- public/app/components/countryInfo/countryInfo.js
+++ public/app/components/countryInfo/countryInfo.js
@@ -27,7 +27,8 @@ angular
     <div>
         <div class="search2">
         <h3>Where are you off to?</h3>
-        <input type="text" ng-model="$ctrl.countryInput" class="searchbar">
+        <input type="text" ng-model="$ctrl.countryInput" class="searchbar" plateholder="Search By Country" ng-keypress="($event.charCode==13)? $ctrl.getCountryData($ctrl.countryInput) : return">
+
         <button ng-cloak class="searchButton" ng-click="$ctrl.getCountryData($ctrl.countryInput); $ctrl.$location.path('/explore'); $ctrl.translationHandler()"> Explore </button>
         </div>
     </div> 
