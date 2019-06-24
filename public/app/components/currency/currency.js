4041bf2577a7e6c6f1f6b6e5f7f1b6a28b1076f8
hannah barker <hannah.m.barker@gmail.com>
2019-06-24 00:06:34 -0400

    trying to figure out white space with mobile toggle tabs

--- public/app/components/currency/currency.js
+++ public/app/components/currency/currency.js
@@ -104,7 +104,7 @@ angular
 .component('currency', {
     controller: CurrencyController,
     template: `
-    <div class="displayContainer"  ng-if="$ctrl.service.countryQueried">
+    <div class="displayContainer" ng-if="$ctrl.service.countryQueried">
                 <h2 class="dataTitle" ng-click="$ctrl.currency=!$ctrl.currency"> Currency Converter </h2>
                    
                     <div class="currencydiv" ng-if="$ctrl.currency"><h3 display="flex">From USD to {{$ctrl.service.currencyNameDisplayArray[0]}}</h3>
